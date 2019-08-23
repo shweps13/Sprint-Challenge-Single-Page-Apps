a7a34a53fd8992d0dc9f344040ceb4836969660d
Heorhii Siburov <g.siburov@gmail.com>
2019-08-23 09:53:15 -0700

    location list styles added

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+import { Card } from 'semantic-ui-react'
+
 export default function LocationCard (props) {
   const { name, type, dimension, residents } = props;
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
-        <p>Type: {type}</p>
-        <p>Dimension: {dimension}</p>
-      </div>
-      <p>{`${residents.length} residents`}</p>
-    </div>
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <br></br>
+        <p>{type} : {dimension}</p>
+      </Card.Content>
+      <Card.Content extra>
+      <p className = "corner">{`${residents.length} residents`}</p>
+      </Card.Content>
+    </Card>
   )
 }
