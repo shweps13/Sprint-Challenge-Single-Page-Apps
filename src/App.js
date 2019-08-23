e2ab005bc6333164e4af702fa96665e24d1c7732
Heorhii Siburov <g.siburov@gmail.com>
2019-08-23 10:34:42 -0700

    'MVP Completed'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
 import CharacterList from "./components/CharacterList"
 import LocationsList from "./components/LocationsList"
 import Episodelist from "./components/EpisodeList"
+import WelcomePage from './components/WelcomePage';
 
 export default function App() {
   return (
     <main>
       
       <Header />
-      {/* <TabNav /> */}
-      <CharacterList />
-      <LocationsList />
-      <Episodelist />
+      <TabNav />
+      <div className="page-view ui bottom attached segment active tab">
+        <Route path='/characters' component={CharacterList} />
+        <Route path='/locations' component={LocationsList} />
+        <Route path='/episodes' component={Episodelist} />
+        <Route exact path='/' component={WelcomePage} />
+      </div>
     </main>
     
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import axios from 'axios';
 import Card from "./CharacterCard";
+import { Link } from "react-router-dom";
 
 export default function CharacterList() {
   const[characterList, setCharacterList] = useState([]);
